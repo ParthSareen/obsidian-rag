dc8d0a40b3f36f4a1fea37d9f04ec893e813e091
ParthSareen <psareen@uwaterloo.ca>
2023-12-11 01:11:07 -0500

    The changes made to the file are:
    1. Added a new section for model selection and loading of the vectorstore using the Hugging Face Transformers library.
    2. Modified the  creation code to use the  method instead of the  method, as it is not needed in this case.
    3. Removed the code for saving the vectorstore since it is not required now.
    4. Updated the  function call to include the retrieval chain with the  argument.
    5. Changed the  variable definition to include the search type as an argument, since it is now used in the  class.

--- obsidian_rag.py
+++ obsidian_rag.py
@@ -12,12 +12,14 @@ from langchain.schema.runnable import RunnablePassthrough
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import chroma
 from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import RetrievalQA
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 
 def get_args() -> str:
     parser: argparse.ArgumentParser = argparse.ArgumentParser(description="Get user file path")
     parser.add_argument('--filepath', help='Input file path')
-    parser.add_argument('--vectorize', default=False, help='Whether to vectorize the file')
+    parser.add_argument('--vectorize', default=False, action=argparse.BooleanOptionalAction, help='Whether to vectorize the file')
     return parser.parse_args() 
 
 def load_markdown_files(directory: str) -> dict:
@@ -30,38 +32,53 @@ def load_markdown_files(directory: str) -> dict:
 def format_docs(docs: list) -> str:
         return "\n\n".join(doc.page_content for doc in docs)
 
+def remove_all_files_in_folder(directory: str) -> None:
+    os.system(f"rm -rf {directory}/*")
 
 def main():
-
+    callbacks = [StreamingStdOutCallbackHandler()]
     args = get_args()
-    loader: ObsidianLoader = ObsidianLoader(path=args.filepath)
-    data: list = loader.load()
-    text_splitter: RecursiveCharacterTextSplitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-    all_splits: list = text_splitter.split_documents(data)
-   
-    # TODO: add a way to save the vectorstore 
+    if args.vectorize:
+        loader: ObsidianLoader = ObsidianLoader(path=args.filepath)
+        data: list = loader.load()
+        text_splitter: RecursiveCharacterTextSplitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+        all_splits: list = text_splitter.split_documents(data)
 
-    vectorstore: chroma.Chroma = chroma.Chroma.from_documents(documents=all_splits, embedding=OllamaEmbeddings(model='mistral'), persist_directory="vectorstore")
-    vectorstore = vectorstore.load()
+        # Hard reset cause LLM be weird
+        remove_all_files_in_folder("vectorstore")
+        # os.mkdir("vectorstore")
 
-    vectorstore.persist()
+        vectorstore: chroma.Chroma = chroma.Chroma.from_documents(documents=all_splits, embedding=OllamaEmbeddings(model='mistral'), persist_directory="vectorstore")
+        print('Vectorized!')
 
+    else:
+        vectorstore: chroma.Chroma = chroma.Chroma(embedding_function=OllamaEmbeddings(model='mistral'), persist_directory="vectorstore")
+        print('Loaded vectorstore!')
+
+    print(vectorstore)
 
     rag_prompt: str = hub.pull("rlm/rag-prompt")
+    print(rag_prompt)
 
-    question: str = "explain cicd?"
-    docs: list = vectorstore.similarity_search(question)
-    print(len(docs))
-    llm = ChatOllama(model="mistral")
-    from langchain.chains import RetrievalQA
+    # question: str = "explain cicd?"
+    # docs: list = vectorstore.similarity_search(question)
+
+    # Document Q&A
+    llm = ChatOllama(model="mistral", callbacks=callbacks)
 
     qa_chain = RetrievalQA.from_chain_type(
         llm,
-        retriever=vectorstore.as_retriever(),
+        retriever=vectorstore.as_retriever(search_kwargs={'k': 3}),
+        # retriever=vectorstore.as_retriever(
+        #     search_type="similarity_score_threshold",
+        #     search_kwargs={'score_threshold': 0.8}
+        # ),
         chain_type_kwargs={"prompt": rag_prompt},
     )
-    ans = qa_chain({"query": question})
-    print(ans)
+    while True:
+        question = input("Ask a question: ")
+        ans = qa_chain({"query": question})
+        print(ans)
     
 
 
